61ca1431ef92729b255bff72328f9102fd47d829
Mohit24680 <mppandey24680@gmail.com>
2024-07-29 17:11:58 +0530

    conversion and operation

---
+++ 03_conversion and operation.js
@@ -0,0 +1,51 @@
+// let score="33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc"=> NaN (not a number)
+// true=1; false=0;
+
+// let IsLoggedIn=""
+// let BooleanIsLoggedIn=Boolean(IsLoggedIn);       
+// console.log(BooleanIsLoggedIn);
+// 1=>true; 0=>false;
+// ""=>false; "Mohit"=>true;
+
+// let someNumber=33;
+// let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log("google");
+
+// ***************operations******************
+// console.log(2**2); //2 ki power 2
+// console.log(2%1); //2 modulo 1 (remainder)
+// let str1="hi"
+// let str2="buddy"
+// let str3=str1+" "+str2;
+// console.log(str3);
+
+// console.log("3" + 2); // 32
+// console.log( 3 + "2"); // 32
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+// console.log( (2+3) * 6 % 2);
+// console.log(1 + 2 + "2" + 2) ; // 322
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
