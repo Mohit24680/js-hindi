6c5344c5c817643933eaa4c822379034cb062e70
Mohit24680 <mppandey24680@gmail.com>
2024-07-29 06:18:00 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+ "use strict";   //it is use to treat all js code as newer version
+//  alert(3+3); //it only works on browser(console),not in nodejs
+console.log(4+4); console.log("mohit") //here the 3 line code is correct and it will run,but try to increase the code readiability.
+
+let name="mohit";
+let age=19;
+let state;
+// number=> 2 to power 53.
+// bigint.
+// string=>"" 
+// boolean=> true ya false
+// null=> standalone value
+// undefined =>
+// symbol=> unique
+
+// object
+console.log(typeof "mohit"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
